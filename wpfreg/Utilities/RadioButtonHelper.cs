ac0a188f35f4b61287c9613c41dd55ae5db1d4b7
soniaa28 <sofija.demchuk@gmail.com>
2023-12-02 20:27:50 +0200

    added radio button helper

---
+++ wpfreg/Utilities/RadioButtonHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace wpfreg.Utilities
+{
+    public static class RadioButtonHelper
+    {
+        public static readonly DependencyProperty UniqueNameProperty =
+            DependencyProperty.RegisterAttached("UniqueName", typeof(string), typeof(RadioButtonHelper));
+
+        public static void SetUniqueName(DependencyObject element, string value)
+        {
+            element.SetValue(UniqueNameProperty, value);
+        }
+
+        public static string GetUniqueName(DependencyObject element)
+        {
+            return (string)element.GetValue(UniqueNameProperty);
+        }
+    }
+}
