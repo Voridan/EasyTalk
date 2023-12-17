ae473172893a240c1f7c640bf9f19572d2820d38
anyavel <anya.velgus99@gmail.com>
2023-12-17 22:33:13 +0200

    fixed input, added handlers on pressing tab button

--- wpfreg/View/LoginView.xaml.cs
+++ wpfreg/View/LoginView.xaml.cs
@@ -27,6 +27,7 @@ namespace wpfreg.View
         private UserService _userService;
         private readonly MainWindow _mainwindow;
         private string nickNamePlaceholder = "Nickname";
+        private string passwordPlaceholder = "Password";
         
       
         public LoginView(UserService userservice, MainWindow mainwindow)
@@ -52,13 +53,24 @@ namespace wpfreg.View
         }
         private void passwordBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            passwordBox.Password = "";
+            if (passwordBox.Password == passwordPlaceholder)
+            {
+                passwordBox.Password = "";
+            }
+            if (textBoxNickname.Text == "")
+            {
+                textBoxNickname.Text = nickNamePlaceholder;
+            }
         }
         private void textBoxNickName_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             if (textBoxNickname.Text == nickNamePlaceholder)
             {
                 textBoxNickname.Text = "";
+            }
+            if (passwordBox.Password == "")
+            {
+                passwordBox.Password = passwordPlaceholder;
             }
         }
         private async void Login(object sender, RoutedEventArgs e)
@@ -99,6 +111,37 @@ namespace wpfreg.View
              registerForm.ShowDialog();
         }
 
-       
+        private void textBoxNickname_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBoxNickname.Text == nickNamePlaceholder)
+            {
+                textBoxNickname.Text = "";
+            }
+        }
+
+        private void textBoxNickname_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (textBoxNickname.Text == "")
+            {
+                textBoxNickname.Text = nickNamePlaceholder;
+            }
+        }
+
+        private void passwordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwordBox.Password == passwordPlaceholder)
+            {
+                passwordBox.Password = "";
+            }
+        }
+
+        private void passwordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwordBox.Password == "")
+            {
+                passwordBox.Password = passwordPlaceholder;
+            }
+        }
+
     }
 }
