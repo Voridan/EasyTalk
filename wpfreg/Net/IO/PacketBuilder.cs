706174d42b3d09b8f383e88161f629920c74f1f8
Vorobienko Bohdan <bogdanvorobienko@gmail.com>
2023-12-17 18:04:09 +0200

    removed not needed code

--- wpfreg/Net/IO/PacketBuilder.cs
+++ wpfreg/Net/IO/PacketBuilder.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace wpfreg.Net.IO
 {
@@ -24,23 +21,10 @@ namespace wpfreg.Net.IO
             var msgLength = msg.Length;
             _ms.Write(BitConverter.GetBytes(msgLength));
             _ms.Write(Encoding.UTF8.GetBytes(msg));
-            bool b = VerifyMessage(msg);
         }
         public byte[] GetPackageBytes()
         {
             return _ms.ToArray();
-        }
-
-        public bool VerifyMessage(string msg)
-        {
-            var expectedLength = msg.Length;
-            var buffer = _ms.ToArray();
-
-            // Assuming the first 4 bytes represent the length of the string
-            var actualLength = BitConverter.ToInt32(buffer, 0);
-
-            // Compare the expected length with the actual length
-            return expectedLength == actualLength;
         }
     }
 }
